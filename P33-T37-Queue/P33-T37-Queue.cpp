3642e904119b8d28d802026db947c30f3722a6d6
lapshuntanya <lapshun.tatiana@gmail.com>
2024-12-04 18:51:18 +0200

    main

--- P33-T37-Queue/P33-T37-Queue.cpp
+++ P33-T37-Queue/P33-T37-Queue.cpp
@@ -1,13 +1,32 @@
 #include "MyQueue.h"
+#include "MyPriorityQueue.h"
 
 int main()
 {
-    myQueue::Queue<int> a;
+   /* myQueue::Queue<int> a;
     a.show();
 
     a.push_back(9);
     a.push_back(7);
     a.push_back(3);
+
+    //Обробка черги
+    while (!a.isEmpty()) {
+        cout << "First: " << a.first() << endl;
+        a.show();
+        cout << endl;
+
+        a.pop_front();
+    }*/
+
+    myQueue::PriorityQueue<int> a;
+
+    a.push_back(6);
+    a.push_back(4);
+    a.push_back(7);
+    a.push_back(5);
+    a.push_back(3);
+    a.show(); // 7 6 5 4 3 
 
     //Обробка черги
     while (!a.isEmpty()) {
