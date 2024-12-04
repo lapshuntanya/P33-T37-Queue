23a8f5c51b3029c73f66313c4d760dc68bd0d759
lapshuntanya <lapshun.tatiana@gmail.com>
2024-12-04 18:09:58 +0200

    Priority Queue

---
+++ P33-T37-Queue/MyPriorityQueue.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "MyQueue.h"
+
+namespace myQueue {
+
+	template<typename U>
+	class PriorityQueue : public Queue<U>
+	{
+	public:
+		void push_back(U value) {
+			Node<U>* el = new Node<U>(value);
+
+			if (Queue<U>::isEmpty()) {
+				Queue<U>::head = Queue<U>::current = el;
+			}
+			else {
+				Node<U>* p = Queue<U>::head;
+				while (p != nullptr && value < p->info)
+					p = p->next;
+
+				if (p == Queue<U>::head) { //Перед головою
+					Queue<U>::head->prev = el;
+					el->next = Queue<U>::head;
+
+					Queue<U>::head = Queue<U>::head->prev;
+				}
+				else if (p == nullptr) { //В кінець черги
+					Queue<U>::current->next = el;
+					el->prev = Queue<U>::current;
+
+					Queue<U>::current = el;
+				}
+				else { //В середину черги
+
+				}
+			}
+		}
+	};
+
+}
--- P33-T37-Queue/P33-T37-Queue.cpp
+++ P33-T37-Queue/P33-T37-Queue.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+#include "MyQueue.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    myQueue::Queue<int> a;
+    a.show();
+
+    a.push_back(9);
+    a.push_back(7);
+    a.push_back(3);
+
+    //Обробка черги
+    while (!a.isEmpty()) {
+        cout << "First: " << a.first() << endl;
+        a.show();
+        cout << endl;
+
+        a.pop_front();
+    }
 }
 
